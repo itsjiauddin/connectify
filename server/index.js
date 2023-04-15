ec2c6b8c09cb9d1744f2e71102bfd155d263ef99
itsjiauddin <jiauddin145@gmail.com>
2023-04-15 19:52:07 +0530

    not serving the frontend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 import express from "express";
-import { fileURLToPath } from "url";
-import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -8,8 +6,6 @@ import { connectDB } from "./config/db.js";
 import { errorHandler } from "./middleware/error.js";
 import authRoutes from "./routes/auth.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 dotenv.config();
 connectDB();
 
@@ -20,8 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/auth", authRoutes);
-
-app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.use(errorHandler);
 
